eb9f4b0f2ad23f9386bab44aacd78ec66f910cbd
UnHa Kim <unha.kim.ghts@gmail.com>
2015-07-07 15:22:12 +0900

    공용 데이터 Go루틴 추가. (테스트는 아직 미작성)
    새로운 I메시지, I질의, I회신을 사용하여 채널을 이용한 Go루틴 형태의 공용 데이터 모듈 작성.
    향후, 테스트 및 디버깅 한 후, zmq 프록시도 작성할 것.

--- shared_data/const.go
+++ shared_data/const.go
@@ -1,10 +1,8 @@
 package shared_data
 
-type T주소_이름 string
-
 const (
-	P주소정보   T주소_이름 = "주소정보"
-	P테스트_결과        = "테스트_결과"
+	P주소정보 string = "주소정보"
+	P테스트_결과 = "테스트_결과"
 
 	P종목정보    = "종목정보"
 	P가격정보    = "가격정보"
