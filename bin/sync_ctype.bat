db5ba726f4ecdea2260d5e470b4faa5963eaa71c
UnHa Kim <unha.kim.ghts@gmail.com>
2015-08-22 11:30:04 +0900

    개발 편의용 배치 파일 수정
    % 누락된 것 추가

--- bin/sync_ctype.bat
+++ bin/sync_ctype.bat
@@ -1,5 +1,7 @@
 @echo off
 
+SET OLDPATH=%PATH%
+
 SET GOARCH=386
 SET CGO_ENABLED=1
 SET GHTS_PATH=%GOPATH%\src\github.com\ghts\ghts
@@ -9,5 +11,9 @@ SET LIBRARY_PATH=%ZMQ_SRC_DIR%\builds\mingw32
 SET GCC=%GHTS_PATH%\3rd_party\ruby_devkit_32
 SET PATH=%GHTS_PATH%\bin;%GHTS_PATH%\3rd_party\NH_OpenAPI;%GCC%\bin;%GCC%\mingw\bin;%ZMQ_SRC_DIR%\builds\mingw32;%PATH%
 
-D:
-cd %GOPATH%\src\github.com\ghts\ghts
+cls
+copy %GHTS_PATH%\api\NH\wmca_ctype.orig %GHTS_PATH%\api\NH\wmca_ctype_orig.go
+go tool cgo -godefs %GHTS_PATH%\api\NH\wmca_ctype_orig.go > %GHTS_PATH%\api\NH\wmca_ctype.go
+del %GHTS_PATH%\api\NH\wmca_ctype_orig.go
+
+SET PATH=%OLDPATH%
--- bin/test-race.bat
+++ bin/test-race.bat
@@ -5,11 +5,11 @@ SET OLDPATH=%PATH%
 SET GOARCH=386
 SET CGO_ENABLED=1
 SET GHTS_PATH=%GOPATH%\src\github.com\ghts\ghts
-SET GCC=%GHTS_PATH%\3rd_party\ruby_devkit_32
 SET ZMQ_SRC_DIR=%GHTS_PATH%\3rd_party\zeromq4-x
 SET C_INCLUDE_PATH=%ZMQ_SRC_DIR%\include
 SET LIBRARY_PATH=%ZMQ_SRC_DIR%\builds\mingw32
-SET PATH=GHTS_PATH\bin;%GCC%\bin;%GCC%\mingw\bin;%ZMQ_SRC_DIR%\builds\mingw32;%PATH%
+SET GCC=%GHTS_PATH%\3rd_party\ruby_devkit_32
+SET PATH=%GHTS_PATH%\bin;%GHTS_PATH%\3rd_party\NH_OpenAPI;%GCC%\bin;%GCC%\mingw\bin;%ZMQ_SRC_DIR%\builds\mingw32;%PATH%
 
 cls
 go test -race
