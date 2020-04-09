6b3fbb264f4124ff1050183852b1fc6be294c6cc
UnHa Kim <unha.kim.ghts@gmail.com>
2020-04-09 18:14:30 +0900

    C/C++ 컴파일러 변경
    Rtools GCC -> msys2 GCC

--- batch_scripts/set_environmental_variables.bat
+++ batch_scripts/set_environmental_variables.bat
@@ -16,9 +16,9 @@ IF NOT DEFINED GOARCH (
 )
 
 IF /I "%GOARCH%"=="amd64" (
-    SET GCC_PATH=C:\Rtools\mingw_64
+    SET GCC_PATH=C:\msys64\mingw_64
 ) ELSE (
-    SET GCC_PATH=C:\Rtools\mingw_32
+    SET GCC_PATH=C:\msys64\mingw_32
 )
 
 SET CGO_ENABLED=1
