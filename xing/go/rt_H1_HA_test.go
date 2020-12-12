e6d3eeffb8b5444f8e809db354d650cc150b257f
UnHa Kim <unha.kim@ghts.org>
2020-12-12 12:43:47 +0900

    불필요한 코멘트 삭제

--- xing/go/rt_H1_HA_test.go
+++ xing/go/rt_H1_HA_test.go
@@ -66,7 +66,6 @@ func TestF호가_잔량_실시간_정보(t *testing.T) {
 	lib.F테스트_같음(t, 종목_ETF.G시장구분(), lib.P시장구분_ETF)
 
 	소켓SUB_실시간 := nano.NewNano소켓SUB_단순형(xt.F주소_실시간())
-	//lib.F대기(lib.P1초)
 
 	lib.F테스트_에러없음(t, F호가_잔량_실시간_정보_구독(종목코드_코스피))
 	lib.F테스트_에러없음(t, F호가_잔량_실시간_정보_구독(종목코드_코스닥))
