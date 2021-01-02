0a25cd9caa0365b7552ae850c755e7035ecd82d7
UnHa Kim <unha.kim@ghts.org>
2021-01-02 17:51:31 +0900

    보안을 위해서 localhost로부터의 접속만을 허용하도록 수정.

--- xing_http/c32/internal/go_http_server.go
+++ xing_http/c32/internal/go_http_server.go
@@ -36,7 +36,9 @@ package x32_http
 import (
 	"github.com/ghts/ghts/lib"
 	xt "github.com/ghts/ghts/xing/base"
+	"net"
 	"net/http"
+	"strings"
 )
 
 func HTTP서버(ch초기화, ch종료 chan lib.T신호) {
@@ -52,5 +54,52 @@ func HTTP서버(ch초기화, ch종료 chan lib.T신호) {
 
 	ch초기화 <- lib.P신호_초기화
 
-	http.ListenAndServe(xt.F주소_C32_호출().G단축값(), nil)
+	//http.ListenAndServe(xt.F주소_C32_호출().G단축값(), nil)
+
+	// 보안을 위해서 localhost로부터의 접속만 허용.
+	// 참고자료 : https://stackoverflow.com/questions/37896931/how-to-limit-client-ip-address-when-using-golang-http-package
+	HTTP서버 := &http.Server{
+		Addr: xt.F주소_C32_호출().G단축값(),
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			// get the real IP of the user
+			addr := getRealAddr(req)
+
+			//lib.F체크포인트(addr)
+
+			// the actual vaildation
+			if addr != "127.0.0.1" {
+				http.Error(w, "Blocked", 401)
+				return
+			}
+
+			// pass the request to the mux
+			http.DefaultServeMux.ServeHTTP(w, req)
+		}),
+	}
+
+	HTTP서버.ListenAndServe()
+}
+
+func getRealAddr(r *http.Request) string {
+	remoteIP := ""
+	// the default is the originating ip. but we try to find better options because this is almost
+	// never the right IP
+	if parts := strings.Split(r.RemoteAddr, ":"); len(parts) == 2 {
+		remoteIP = parts[0]
+	}
+	// If we have a forwarded-for header, take the address from there
+	if xff := strings.Trim(r.Header.Get("X-Forwarded-For"), ","); len(xff) > 0 {
+		addrs := strings.Split(xff, ",")
+		lastFwd := addrs[len(addrs)-1]
+		if ip := net.ParseIP(lastFwd); ip != nil {
+			remoteIP = ip.String()
+		}
+		// parse X-Real-Ip header
+	} else if xri := r.Header.Get("X-Real-Ip"); len(xri) > 0 {
+		if ip := net.ParseIP(xri); ip != nil {
+			remoteIP = ip.String()
+		}
+	}
+
+	return remoteIP
 }
