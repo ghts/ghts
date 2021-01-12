c68583bfbbbce34cbc152f5b5206d167f9afece9
UnHa Kim <unha.kim@ghts.org>
2021-01-12 22:20:37 +0900

    xing http를 위한 주소 출력 비활성화.

--- xing/base/func_helper.go
+++ xing/base/func_helper.go
@@ -34,8 +34,6 @@ along with GHTS.  If not, see <http://www.gnu.org/licenses/>. */
 package xt
 
 import (
-	"bytes"
-	"fmt"
 	"github.com/ghts/ghts/lib"
 	"os"
 	"reflect"
@@ -129,16 +127,16 @@ func F주소_설정() {
 			os.Setenv(P주소_C32_콜백_환경변수명, strconv.Itoa(int(주소_C32_콜백)))
 			os.Setenv(P주소_실시간_환경변수명, strconv.Itoa(int(주소_실시간)))
 
-			버퍼 := bytes.Buffer{}
-			문자열 := lib.F2문자열("* Xing API 호출 URL : '%v' *", F주소_C32_호출().URL())
-
-			for i := 0; i < len(문자열)-3; i++ {
-				버퍼.WriteString("*")
-			}
-
-			fmt.Println(버퍼.String())
-			fmt.Println(문자열)
-			fmt.Println(버퍼.String())
+			//버퍼 := bytes.Buffer{}
+			//문자열 := lib.F2문자열("* Xing API 호출 URL : '%v' *", F주소_C32_호출().URL())
+			//
+			//for i := 0; i < len(문자열)-3; i++ {
+			//	버퍼.WriteString("*")
+			//}
+			//
+			//fmt.Println(버퍼.String())
+			//fmt.Println(문자열)
+			//fmt.Println(버퍼.String())
 
 			return
 		}
