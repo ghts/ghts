12ad4b837d8e04c15bebb5049e8cd5f9219d95d2
UnHa Kim <unha.kim@ghts.org>
2021-01-17 20:29:10 +0900

    불필요한 파일 삭제

--- batch_scripts/dependency_install.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-call %GOPATH%\src\github.com\ghts\ghts\batch_scripts\set_environmental_variables.bat
-
-go get -u github.com/pebbe/zmq3
-go get -u github.com/ugorji/go/codec
-go get -u golang.org/x/text
--- batch_scripts/g.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-call %GOPATH%\src\github.com\ghts\ghts\batch_scripts\set_environmental_variables.bat
-
-cd %PROJECT_ROOT%
