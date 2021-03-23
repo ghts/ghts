a03c74f5373bea922e1a46f56458e1667368b880
KUH <unha.kim@ghts.org>
2021-03-23 09:22:46 +0900

    type_sync.bat 디버깅 문자열 삭제.

--- xing/base/type_sync.bat
+++ xing/base/type_sync.bat
@@ -1,12 +1,8 @@
 @echo off
 
-ECHO 1
-
 IF NOT DEFINED GOPATH (
     SET GOPATH=%USERPROFILE%\Go
 )
-
-ECHO 2
 
 REM *********** 
 REM *  32Bit  *
@@ -14,28 +10,13 @@ REM ***********
 
 call %GOPATH%\src\github.com\ghts\ghts\batch_scripts\32.bat
 
-ECHO 3
-
 REM cls
 cd %GOPATH%\src\github.com\ghts\ghts\xing\base
 
-ECHO 4
-
 copy type_c.orig type_1.go >NUL
-
-ECHO 5
-
 "%GOROOT%\bin\go.exe" tool cgo -godefs type_1.go > type_2.go
-
-ECHO 6
-
 sed -e 's/uint8/byte/g' type_2.go > type_3.go
-
-ECHO 7
-
 sed -e 's/int8/byte/g' type_3.go > type_c.go
-
-ECHO 8
 
 del type_1.go
 del type_2.go
