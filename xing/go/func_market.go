3c0e48d1193b0a46e974280bfd2179bdff2b5131
KUH <unha.kim@ghts.org>
2021-11-30 23:45:30 +0900

    xing.F코스피_종목_여부(), xing.F코스닥_종목_여부() 추가.

--- xing/go/func_market.go
+++ xing/go/func_market.go
@@ -365,6 +365,22 @@ func f임의_종목_추출(종목_모음 []*lib.S종목) *lib.S종목 {
 	return 종목_모음[lib.F임의_범위_이내_정수값(0, len(종목_모음))].G복제본()
 }
 
+func F코스피_종목_여부(종목_코드 string) bool {
+	if 종목, 에러 := F종목by코드(종목_코드); 에러 != nil {
+		return false
+	} else {
+		return 종목.G시장구분() == lib.P시장구분_코스피
+	}
+}
+
+func F코스닥_종목_여부(종목_코드 string) bool {
+	if 종목, 에러 := F종목by코드(종목_코드); 에러 != nil {
+		return false
+	} else {
+		return 종목.G시장구분() == lib.P시장구분_코스닥
+	}
+}
+
 func ETF_ETN_종목_여부(종목_코드 string) bool {
 	종목, 에러 := F종목by코드(종목_코드)
 
