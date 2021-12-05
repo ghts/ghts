6b3183612e9282b6553de6c500dba08a582fc73d
KUH <unha.kim@ghts.org>
2021-12-05 14:34:16 +0900

    헤더 파일 HB_ 추가 (코스닥 시간외 호가 잔량)

--- xing/base/type_c.h
+++ xing/base/type_c.h
@@ -3103,6 +3103,18 @@ typedef struct {
 } HA_OutBlock;
 
 //------------------------------------------------------------------------------
+// 코스닥 시간외 호가 잔량 (HB_)
+//------------------------------------------------------------------------------
+typedef struct {
+    char    hotime              [   6];    char    _hotime              ;    // [string,    6] 호가시간                        StartPos 0, Length 6
+    char    tmofferrem          [  12];    char    _tmofferrem          ;    // [long  ,   12] 시간외매도잔량                  StartPos 7, Length 12
+    char    tmbidrem            [  12];    char    _tmbidrem            ;    // [long  ,   12] 시간외매수잔량                  StartPos 20, Length 12
+    char    pretmoffercha       [  12];    char    _pretmoffercha       ;    // [long  ,   12] 시간외매도수량직전대비          StartPos 33, Length 12
+    char    pretmbidcha         [  12];    char    _pretmbidcha         ;    // [long  ,   12] 시간외매수수량직전대비          StartPos 46, Length 12
+    char    shcode              [   6];    char    _shcode              ;    // [string,    6] 단축코드                        StartPos 59, Length 6
+} HB_OutBlock;
+
+//------------------------------------------------------------------------------
 // 코스피 체결 (S3_)
 //------------------------------------------------------------------------------
 typedef struct {
