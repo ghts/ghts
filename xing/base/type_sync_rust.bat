0b5b20b42522798c50b9f45764465d0bff220651
UnHa Kim <ghts@users.noreply.github.com>
2022-06-12 05:52:26 +0900

    32비트 인수 추가.

--- xing/base/type_sync_rust.bat
+++ xing/base/type_sync_rust.bat
@@ -2,12 +2,15 @@
 
 cls
 
+REM 32비트에 맞게 하려면.
+REM >rustup target add i686-pc-windows-msvc
+
 SET LIBCLANG_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Tools\Llvm\x64\bin
 SET PATH_ORIG=%PATH%
 SET PATH=%PATH%;C:\Program Files\Git\usr\bin
 
 cd %GOPATH%\src\github.com\ghts\ghts\xing\base
 
-"%HOMEPATH%\.cargo\bin\bindgen.exe" type_c.h -o type_c.rs --no-layout-tests
+"%HOMEPATH%\.cargo\bin\bindgen.exe"  --no-layout-tests type_c.h -o type_c.rs -- --target=i686-pc-windows-msvc
 
 SET PATH=%PATH_ORIG%
