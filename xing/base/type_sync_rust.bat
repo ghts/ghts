c39a688575f77c9c70a341ce8e285fb6f7cc863e
UnHa Kim <ghts@users.noreply.github.com>
2022-06-12 05:22:54 +0900

    Rust 언어 바인딩 코드 단순화.
    이해 안 되는 테스트 코드들 제거.
    
    불필요한 강제 sed 변환 삭제.

--- xing/base/type_sync_rust.bat
+++ xing/base/type_sync_rust.bat
@@ -1,15 +1,13 @@
 @echo off
 
-REM ***********
-REM *  32Bit  *
-REM ***********
-
 cls
 
-REM call %GOPATH%\src\github.com\ghts\ghts\batch_scripts\32.bat
-REM SET LIBCLANG_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Tools\Llvm\bin
 SET LIBCLANG_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Tools\Llvm\x64\bin
+SET PATH_ORIG=%PATH%
+SET PATH=%PATH%;C:\Program Files\Git\usr\bin
 
 cd %GOPATH%\src\github.com\ghts\ghts\xing\base
 
-"%HOMEPATH%\.cargo\bin\bindgen.exe" type_c.h -o type_c.rs
+"%HOMEPATH%\.cargo\bin\bindgen.exe" type_c.h -o type_c.rs --no-layout-tests
+
+SET PATH=%PATH_ORIG%
