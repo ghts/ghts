5571e13aae584fe5066b34b9326569edbf0c1d87
KUH <unha.kim.ghts@gmail.com>
2022-11-08 21:03:57 +0900

    F당일_일일_가격정보_수집() 버그 수정.

--- xing/util/daily_data.go
+++ xing/util/daily_data.go
@@ -34,7 +34,7 @@ func F당일_일일_가격정보_수집(db *sql.DB) (에러 error) {
 		s := new(dpd.S종목별_일일_가격정보_모음)
 		s.DB읽기with시작일(db, 종목코드, 한달전)
 
-		if s.M저장소[len(s.M저장소)-1].M일자 == 당일 {
+		if len(s.M저장소) > 0 && s.M저장소[len(s.M저장소)-1].M일자 == 당일 {
 			continue
 		}
 
