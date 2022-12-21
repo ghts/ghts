b7f519bbb445fe7bd70b6d332a20181508e26d7b
UnHa Kim <ghts@users.noreply.github.com>
2022-12-21 16:29:56 +0900

    백테스트용 시가총액 데이터 관련 코드 정리

--- backtest/factor/common/func_bt_common.go
+++ backtest/factor/common/func_bt_common.go
@@ -1,10 +1,46 @@
-package bt_factor_common
+package bfc //bt_factor_common
 
 import (
+	"bytes"
+	"database/sql"
 	"github.com/ghts/ghts/lib"
 	"path"
 )
 
 func DB파일명() string {
 	return path.Join(lib.F홈_디렉토리(), "backtest_factor.dat")
+}
+
+func F시가총액_테이블_생성(db *sql.DB) (에러 error) {
+	defer lib.S예외처리{M에러: &에러}.S실행()
+
+	SQL := new(bytes.Buffer)
+	SQL.WriteString("CREATE TABLE IF NOT EXISTS marcap (")
+	SQL.WriteString("code CHAR(8) NOT NULL,")
+	SQL.WriteString("date DATE NOT NULL,")
+	SQL.WriteString("name VARCHAR(255) NOT NULL,")
+	SQL.WriteString("market CHAR(3) NOT NULL,")
+	SQL.WriteString("open DECIMAL(20,3) NOT NULL,")
+	SQL.WriteString("high DECIMAL(20,3) NOT NULL,")
+	SQL.WriteString("low DECIMAL(20,3) NOT NULL,")
+	SQL.WriteString("close DECIMAL(20,3) NOT NULL,")
+	SQL.WriteString("volume BIGINT NOT NULL,")
+	SQL.WriteString("amount BIGINT NOT NULL,")
+	SQL.WriteString("listed_qty BIGINT NOT NULL,")
+	SQL.WriteString("cap_mil BIGINT NOT NULL,")
+	SQL.WriteString("cap_rank INT NOT NULL,")
+	SQL.WriteString("CONSTRAINT pk_marcap PRIMARY KEY (code,date)")
+	SQL.WriteString(")")
+
+	_, 에러 = db.Exec(SQL.String())
+
+	return 에러
+}
+
+func F시가총액_테이블_삭제(db *sql.DB) (에러 error) {
+	defer lib.S예외처리{M에러: &에러}.S실행()
+
+	_, 에러 = db.Exec("DROP TABLE IF EXISTS marcap")
+
+	return 에러
 }
--- backtest/factor/data/marcap.go
+++ backtest/factor/data/marcap.go
@@ -24,8 +24,8 @@ func F시총_데이터_저장() (에러 error) {
 	}
 
 	db := lib.F확인2(sql.Open("sqlite", bfc.DB파일명()))
-	//lib.F확인1(F시가총액_테이블_삭제(db))
-	lib.F확인1(F시가총액_테이블_생성(db))
+	//lib.F확인1(bfc.F시가총액_테이블_삭제(db))
+	lib.F확인1(bfc.F시가총액_테이블_생성(db))
 	lib.F확인1(db저장(db, 레코드_모음))
 
 	return nil
@@ -126,38 +126,4 @@ func db저장(db *sql.DB, 레코드_모음 [][]string) (에러 error) {
 	}
 
 	return tx.Commit()
-}
-
-func F시가총액_테이블_삭제(db *sql.DB) (에러 error) {
-	defer lib.S예외처리{M에러: &에러}.S실행()
-
-	_, 에러 = db.Exec("DROP TABLE IF EXISTS marcap")
-
-	return 에러
-}
-
-func F시가총액_테이블_생성(db *sql.DB) (에러 error) {
-	defer lib.S예외처리{M에러: &에러}.S실행()
-
-	SQL := new(bytes.Buffer)
-	SQL.WriteString("CREATE TABLE IF NOT EXISTS marcap (")
-	SQL.WriteString("code CHAR(8) NOT NULL,")
-	SQL.WriteString("date DATE NOT NULL,")
-	SQL.WriteString("name VARCHAR(255) NOT NULL,")
-	SQL.WriteString("market CHAR(3) NOT NULL,")
-	SQL.WriteString("open DECIMAL(20,3) NOT NULL,")
-	SQL.WriteString("high DECIMAL(20,3) NOT NULL,")
-	SQL.WriteString("low DECIMAL(20,3) NOT NULL,")
-	SQL.WriteString("close DECIMAL(20,3) NOT NULL,")
-	SQL.WriteString("volume BIGINT NOT NULL,")
-	SQL.WriteString("amount BIGINT NOT NULL,")
-	SQL.WriteString("listed_qty BIGINT NOT NULL,")
-	SQL.WriteString("cap_mil BIGINT NOT NULL,")
-	SQL.WriteString("cap_rank INT NOT NULL,")
-	SQL.WriteString("CONSTRAINT pk_marcap PRIMARY KEY (code,date)")
-	SQL.WriteString(")")
-
-	_, 에러 = db.Exec(SQL.String())
-
-	return 에러
 }
