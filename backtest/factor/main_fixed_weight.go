dc59a648ce7104846fb3a498898e9c33d0f07ddb
UnHa Kim <ghts@users.noreply.github.com>
2023-01-08 10:23:32 +0900

    변경된 패키지 이름 반영.

--- backtest/factor/main_fixed_weight.go
+++ backtest/factor/main_fixed_weight.go
@@ -3,6 +3,6 @@ package main
 import "github.com/ghts/ghts/backtest/factor/fixed_weight"
 
 func main() {
-	전략_인수 := bt.New전략_인수_세종(bt.F점수_계산_강환국_무작정_따라하기_단순형)
-	bt.F백테스트_실행(전략_인수)
+	전략_인수 := ftfw.New전략_인수_세종(ftfw.F점수_계산_강환국_무작정_따라하기_단순형)
+	ftfw.F백테스트_실행(전략_인수)
 }
