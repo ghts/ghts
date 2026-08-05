c7920f815576a2541905afa92a0ec35ec59f9ddc
UnHa Kim <ghts@users.noreply.github.com>
2026-08-05 12:03:07 +0900

    CSPAQ12300 테스트 에러 수정
    각 멤버 필드값의 의미를 이해하지 못해서 값이 정확한 지 검증은 못함.
    미결제수량 음수로 나옴.

--- xing/base/tr_CSPAQ12300.go
+++ xing/base/tr_CSPAQ12300.go
@@ -3,9 +3,10 @@ package xt
 import (
 	"bytes"
 	"encoding/binary"
-	lb "github.com/ghts/ghts/lib"
 	"strings"
 	"time"
+
+	lb "github.com/ghts/ghts/lib"
 )
 
 type CSPAQ12300_현물계좌_잔고내역_질의값 struct {
